a8d6ed39eb178afdb19544bcf69525949b394481
marepifanio <marcelog.epifanio@gmail.com>
2023-06-27 21:23:31 -0300

    Tente-isto 3-2. modificado mecanismo de deleção de do item, adicionado mais informações

--- help/src/Help.java
+++ help/src/Help.java
@@ -7,17 +7,24 @@ import java.io.IOException;
     */
 public class Help {
     public static void main(String[] args) throws IOException {
-        char choice;
+        char choice, ignore;
 
-        System.out.println("Help on:");
-        System.out.println("\t1. if");
-        System.out.println("\t2. switch");
-        System.out.println("Choose one: ");
+        do {
+            System.out.println("Help on:");
+            System.out.println("\t1. if");
+            System.out.println("\t2. switch");
+            System.out.println("\t3. for");
+            System.out.println("\t4. while");
+            System.out.println("\t5. do-while\n");
+            System.out.println("Choose one: ");
 
-        choice = (char) System.in.read();
-        System.out.println("\n");
+            choice = (char) System.in.read();
+            do {
+                ignore = (char) System.in.read();
+            }while (ignore != '\n');
+        }while (choice < '1' | choice > '5');
 
-            switch (choice) {
+        switch (choice) {
             case '1':
                 System.out.println("The if:\n");
                 System.out.println("if(condition) statement;");
@@ -32,8 +39,21 @@ public class Help {
                 System.out.println("\t// ...");
                 System.out.println("}");
                 break;
-            default:
-                System.out.println("Selection not found.");
+            case '3':
+                System.out.println("The for:\n");
+                System.out.print("for(init; condition; interation) ");
+                System.out.println("statement;");
+                break;
+            case '4':
+                System.out.println("The while:\n");
+                System.out.println("while(condition) statement;");
+                break;
+            case '5':
+                System.out.println("The do-while:\n");
+                System.out.println("do {");
+                System.out.println("\tstatement;");
+                System.out.println("} while (condition);");
+                break;
         }
     }
 }
